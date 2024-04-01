c45fb40edc0553e4e5e6cde57c029c76de7de144
유민식 <soule125@naver.com>
2024-04-01 23:32:16 +0900

    add schedule/getAllSchedule 1차 완성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,79 +1,86 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import Joi from 'joi';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-import { AuthModule } from './auth/auth.module';
-import { PostsModule } from './posts/posts.module';
-import { UsersModule } from './users/users.module';
-import { GroupsModule } from './groups/groups.module';
-import { Users } from './users/entities/user.entity';
-import { SchedulesModule } from './schedules/schedules.module';
-import { ScheduleMembersModule } from './schedule-members/schedule-members.module';
-import { PostCommentsModule } from './post-comments/post-comments.module';
-import { FollowsModule } from './follows/follows.module';
-import { RecordsModule } from './records/records.module';
-import { GroupMembersModule } from './group-members/group-members.module';
-import { Groups } from './groups/entities/group.entity';
-import { GroupMembers } from './group-members/entities/group-member.entity';
-import { MailModule } from './mail/mail.module';
-import { ENV_DB_HOST, ENV_DB_NAME, ENV_DB_PASSWORD, ENV_DB_PORT, ENV_DB_SYNC, ENV_DB_USERNAME } from './const/env.keys';
-import { Posts } from './posts/entities/posts.entity';
-import { PostComments } from './post-comments/entities/post-comment.entity';
-import { Schedules } from './schedules/entities/schedule.entity';
-import { ScheduleMembers } from './schedule-members/entities/schedule-member.entity';
-import { Records } from './records/entities/record.entity';
-import { Follows } from './follows/entities/follow.entity';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+// // import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+// import Joi from 'joi';
+// // import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+// import { AuthModule } from './auth/auth.module';
+// import { PostsModule } from './posts/posts.module';
+// import { UsersModule } from './users/users.module';
+// import { GroupsModule } from './groups/groups.module';
+// // import { Users } from './users/entities/user.entity';
+// import { SchedulesModule } from './schedules/schedules.module';
+// import { ScheduleMembersModule } from './schedule-members/schedule-members.module';
+// import { PostCommentsModule } from './post-comments/post-comments.module';
+// import { FollowsModule } from './follows/follows.module';
+// import { RecordsModule } from './records/records.module';
+// import { GroupMembersModule } from './group-members/group-members.module';
+// // import { Groups } from './groups/entities/group.entity';
+// // import { GroupMembers } from './group-members/entities/group-member.entity';
+// import { MailModule } from './mail/mail.module';
+// import {} from // ENV_DB_HOST,
+// // ENV_DB_NAME,
+// // ENV_DB_PASSWORD,
+// // ENV_DB_PORT,
+// // ENV_DB_SYNC,
+// // ENV_DB_USERNAME,
+// './const/env.keys';
+// // import { Posts } from './posts/entities/posts.entity';
+// // import { PostComments } from './post-comments/entities/post-comment.entity';
+// // import { Schedules } from './schedules/entities/schedule.entity';
+// // import { ScheduleMembers } from './schedule-members/entities/schedule-member.entity';
+// // import { Records } from './records/entities/record.entity';
+// // import { Follows } from './follows/entities/follow.entity';
 
-const typeOrmModuleOptions = {
-  useFactory: async (
-    configService: ConfigService,
-  ): Promise<TypeOrmModuleOptions> => ({
-    namingStrategy: new SnakeNamingStrategy(),
-    type: 'postgres',
-    username: configService.get<string>(ENV_DB_USERNAME),
-    password: configService.get<string>(ENV_DB_PASSWORD),
-    host: configService.get<string>(ENV_DB_HOST),
-    port: configService.get<number>(ENV_DB_PORT),
-    database: configService.get<string>(ENV_DB_NAME),
-    entities: [ Users, Groups, GroupMembers, Posts, PostComments, Schedules, ScheduleMembers, Records, Follows ],
-    synchronize: configService.get<boolean>(ENV_DB_SYNC),
-    logging: true,
-  }),
-  inject: [ConfigService],
-};
+// // const typeOrmModuleOptions = {
+// //   useFactory: async (
+// //     configService: ConfigService,
+// //   ): Promise<TypeOrmModuleOptions> => ({
+// //     namingStrategy: new SnakeNamingStrategy(),
+// //     type: 'postgres',
+// //     username: configService.get<string>(ENV_DB_USERNAME),
+// //     password: configService.get<string>(ENV_DB_PASSWORD),
+// //     host: configService.get<string>(ENV_DB_HOST),
+// //     port: configService.get<number>(ENV_DB_PORT),
+// //     database: configService.get<string>(ENV_DB_NAME),
+// //     entities: [ Users, Groups, GroupMembers, Posts, PostComments, Schedules, ScheduleMembers, Records, Follows ],
+// //     synchronize: configService.get<boolean>(ENV_DB_SYNC),
+// //     logging: true,
+// //   }),
+// //   inject: [ConfigService],
+// // };
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    validationSchema: Joi.object({
-      JWT_SECRET_KEY: Joi.string().required(),
-      PASSWORD_HASH_ROUNDS: Joi.number().required(),
-      ROLE_ADMIN_PASSWORD: Joi.string().required(),
-      MAILER_EMAIL: Joi.string().required(),
-      MAILER_PASSWORD: Joi.string().required(),
-      DB_USERNAME: Joi.string().required(),
-      DB_PASSWORD: Joi.string().required(),
-      DB_HOST: Joi.string().required(),
-      DB_PORT: Joi.number().required(),
-      DB_NAME: Joi.string().required(),
-      DB_SYNC: Joi.boolean().required()
-    }),
-  }),
-  TypeOrmModule.forRootAsync(typeOrmModuleOptions),
-  AuthModule,
-  PostsModule,
-  UsersModule,
-  GroupsModule,
-  SchedulesModule,
-  ScheduleMembersModule,
-  PostCommentsModule,
-  FollowsModule,
-  RecordsModule,
-  GroupMembersModule,
-  MailModule,
-],
-  controllers: [],
-  providers: [],
+  // imports: [
+  //   ConfigModule.forRoot({
+  //     isGlobal: true,
+  //     validationSchema: Joi.object({
+  //       JWT_SECRET_KEY: Joi.string().required(),
+  //       PASSWORD_HASH_ROUNDS: Joi.number().required(),
+  //       ROLE_ADMIN_PASSWORD: Joi.string().required(),
+  //       MAILER_EMAIL: Joi.string().required(),
+  //       MAILER_PASSWORD: Joi.string().required(),
+  //       DB_USERNAME: Joi.string().required(),
+  //       DB_PASSWORD: Joi.string().required(),
+  //       DB_HOST: Joi.string().required(),
+  //       DB_PORT: Joi.number().required(),
+  //       DB_NAME: Joi.string().required(),
+  //       DB_SYNC: Joi.boolean().required(),
+  //     }),
+  //   }),
+  //   TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+  //   AuthModule,
+  //   PostsModule,
+  //   UsersModule,
+  //   GroupsModule,
+  //   SchedulesModule,
+  //   ScheduleMembersModule,
+  //   PostCommentsModule,
+  //   FollowsModule,
+  //   RecordsModule,
+  //   GroupMembersModule,
+  //   MailModule,
+  // ],
+  // controllers: [],
+  // providers: [],
 })
 export class AppModule {}
--- src/schedules/schedules.service.ts
+++ src/schedules/schedules.service.ts
@@ -22,6 +22,7 @@ export class SchedulesService {
       scheduleDate,
     });
 
+    // 매개변수로 받은 groupId로 그룹을 찾는다.
     const group = await this.ScheduleRepository.find({
       where: { groupId },
     });
@@ -38,4 +39,12 @@ export class SchedulesService {
       message: '스케쥴을 작성하였습니다.',
     };
   }
+
+  // 스케쥴 전체 조회
+  /** 클라이언트가 url에 접근하면 자동적으로 싹 보여줌... 그럼 파라미터는 없어도 되는 거 아닌가?**/
+  async getAllSchedule() {
+    return await this.ScheduleRepository.find({});
+  }
+
+  // 스케쥴 상세 조회
 }
