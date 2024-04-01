c204c0d048e986748e43075cc62e050335f883bd
유민식 <soule125@naver.com>
2024-04-01 11:35:52 +0900

    Create branch

--- src/schedules/entities/schedule.entity.ts
+++ src/schedules/entities/schedule.entity.ts
@@ -1,51 +1,60 @@
-import { Groups } from "src/groups/entities/group.entity";
-import { ScheduleMembers } from "src/schedule-members/entities/schedule-member.entity";
-import { Users } from "src/users/entities/user.entity";
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Groups } from 'src/groups/entities/group.entity';
+import { ScheduleMembers } from 'src/schedule-members/entities/schedule-member.entity';
+import { Users } from 'src/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity({
-    name : 'schedules'
+  name: 'schedules',
 })
-
 export class Schedules {
-    
-    @PrimaryGeneratedColumn()
-    scheduleId : number;
+  @PrimaryGeneratedColumn()
+  scheduleId: number;
 
-    @Column({ type : 'varchar', nullable : false })
-    title : string;
+  @Column({ type: 'varchar', nullable: false })
+  title: string;
 
-    @Column({ type : 'text', nullable : false })
-    content : string;
+  @Column({ type: 'text', nullable: false })
+  content: string;
 
-    @Column({ type : 'date', nullable : false })
-    scheduleDate : Date;
-    
-    @CreateDateColumn({ type : 'timestamp' })
-    createdAt : Date;
+  @Column({ type: 'date', nullable: false })
+  scheduleDate: Date;
 
-    @UpdateDateColumn({ type : 'timestamp' })
-    updatedAt : Date;
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
 
-    @ManyToOne(() => Groups, (groups) => groups.schedules, {
-        onDelete : 'CASCADE'
-    })
-    @JoinColumn({ name : 'groupId', referencedColumnName : 'groupId' })
-    groups : Groups;
-    
-    @Column({ type : 'int', nullable : false })
-    groupId : number;
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
 
-    @ManyToOne(() => Users, (users) => users.schedules, {
-        onDelete : 'CASCADE'
-    })
-    @JoinColumn({ name : 'userId', referencedColumnName : 'userId' })
-    users : Users;
+  @ManyToOne(() => Groups, (groups) => groups.schedules, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'groupId', referencedColumnName: 'groupId' })
+  groups: Groups;
 
-    @Column({ type : 'int', nullable : false })
-    userId : number;
+  @Column({ type: 'int', nullable: false })
+  groupId: number;
 
-    @OneToMany(() => ScheduleMembers, (scheduleMembers) => scheduleMembers.schedules)
-    scheduleMembers : ScheduleMembers[];
-    
+  @ManyToOne(() => Users, (users) => users.schedules, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'userId', referencedColumnName: 'userId' })
+  users: Users;
+
+  @Column({ type: 'int', nullable: false })
+  userId: number;
+
+  @OneToMany(
+    () => ScheduleMembers,
+    (scheduleMembers) => scheduleMembers.schedules,
+  )
+  scheduleMembers: ScheduleMembers[];
 }
