12abab31155a4692958f9a5b678c14fb91f64345
dsad233 <ksl1421@naver.com>
2024-04-01 18:13:40 +0900

    add : git-member 부분 추가

--- src/group-members/group-members.controller.ts
+++ src/group-members/group-members.controller.ts
@@ -4,17 +4,25 @@ import { CreateGroupMemberDto } from './dto/create-group-member.dto';
 import { UpdateGroupMemberDto } from './dto/update-group-member.dto';
 import { User } from 'src/utils/userInfo.decorator';
 import { Users } from 'src/users/entities/user.entity';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 
 @Controller('group-members')
 export class GroupMembersController {
   constructor(private readonly groupMembersService: GroupMembersService) {}
 
+
+  @ApiTags('group-members')
+  @ApiOperation({ summary: '그룹 맴버 모든 목록 조회 API', description: '그룹의 권한 있는 목록만 조회' })
+  @ApiCreatedResponse({ description: '그룹 맴버 조회'})
   @Get()
   findAll() {
     return this.groupMembersService.findAll();
   }
 
+  @ApiTags('group-members')
+  @ApiOperation({ summary: '그룹 맴버 상세 조회 API', description: '그룹의 권한 있는 목록만 조회' })
+  @ApiCreatedResponse({ description: '그룹 맴버 조회'})
   @Get(':id')
   findOne(@Param('id') groupMemberId: number) {
     return this.groupMembersService.findOne(groupMemberId);
