85bf68e97df0958c8d08c59d4b13c89552f5894c
dsad233 <ksl1421@naver.com>
2024-04-28 15:36:05 +0900

    Fix : 병합 오류 수정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,6 @@ import { Users } from 'src/users/entities/user.entity';
 import { ENV_JWT_SECRET_KEY } from 'src/const/env.keys';
 import { JwtStrategy } from './strategy/jwt.strategy';
 import { RoleStrategy } from './strategy/roles.strategy';
-import { RolesGuard } from './guard/roles.guard';
 
 @Module({
   imports: [
