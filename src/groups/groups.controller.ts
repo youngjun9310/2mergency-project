f781d617f0b74fe54a8f3e37c3be4c6550b6df8b
dsad233 <ksl1421@naver.com>
2024-04-28 09:21:54 +0900

    Fix : hbs 수정

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -26,9 +26,8 @@ export class GroupsController {
   }
 
   // 그룹 모든 목록 조회 //
-  @ApiOperation({ summary: '모든 그룹 목록 조회 API', description: '모든 그룹 목록 조회 성공' })
-  @ApiResponse({ status: 200, description: '성공적으로 모든 그룹 목록이 조회되었습니다.' })
   @ApiOperation({ summary: '모든 그룹 조회 API', description: '모든 그룹 목록 조회' })
+  @ApiResponse({ description: '성공적으로 모든 그룹 목록을 조회하였습니다.', status: 200 })
   @Get('')
   async findAllGroups() {
     return await this.groupsService.findAllGroups();
@@ -37,7 +36,6 @@ export class GroupsController {
   // 그룹 상세 조회 //
   @ApiOperation({ summary: '그룹 상세 조회 API', description: '그룹 상세 정보 조회 성공' })
   @ApiResponse({ status: 200, description: '성공적으로 그룹의 상세 정보를 조회하였습니다.' })
-  @ApiBearerAuth('access-token')
   @Get('')
   async findOneGroup(@Param('groupId', ParseIntPipe) groupId: number) {
     return this.groupsService.findOneGroup(groupId);
@@ -48,8 +46,8 @@ export class GroupsController {
   @MemberRoles(MemberRole.Admin, MemberRole.Main)
   @ApiOperation({ summary: '그룹 수정', description: '그룹의 목록 수정 성공' })
   @ApiResponse({ status: 200, description: '성공적으로 그룹을 수정하였습니다.' })
-  @Patch(':groupId')
   @ApiBearerAuth('access-token')
+  @Patch(':groupId')
   async updateGroup(@Param('groupId') groupId: number, @Body() updateGroupDto: UpdateGroupDto) {
     return await this.groupsService.updateGroup(groupId, updateGroupDto);
   }
@@ -57,10 +55,9 @@ export class GroupsController {
   // 그룹 삭제 //
   @UseGuards(JWTAuthGuard, memberRolesGuard)
   @MemberRoles(MemberRole.Admin, MemberRole.Main)
-  @ApiOperation({ summary: '그룹 삭제 API', description: '그룹 삭제 성공' })
-  @ApiResponse({ status: 204, description: '성공적으로 그룹을 삭제하였습니다.' })
+  @ApiOperation({ summary: '그룹 삭제 API', description: '그룹 삭제' })
+  @ApiResponse({ description: '성공적으로 그룹을 삭제하였습니다.', status: 204 })
   @Delete(':groupId')
-  @ApiBearerAuth('access-token')
   async deleteGroup(@Param('groupId') groupId: number) {
     return await this.groupsService.deleteGroup(groupId);
   }
